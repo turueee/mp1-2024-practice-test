c7be7f449e4f78be3aec374f05dbd3fe0d41dfd1
turueee <turushkine@inbox.ru>
2024-12-19 10:32:26 +0300

    Update task1.c

--- 3824Б1ФИ1/27 Турушкин Егор Антонович/task1.c
+++ 3824Б1ФИ1/27 Турушкин Егор Антонович/task1.c
@@ -3,5 +3,18 @@
 // двумя нулями.
 int task1(int A[], size_t n)
 {
-  return -1;
+  int i = 0, counter = 0;
+  for (i = 0; i < n; i++)
+  {
+    if (A[i] == 0)
+    {
+      while (A[i + 1] != 0)
+      {
+        i++;
+        counter += A[i];
+      }
+      return counter;
+    }
+  }
+
 }
