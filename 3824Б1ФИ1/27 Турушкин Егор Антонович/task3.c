0776cb1d276ff2ace2e2f988148228f57497eccd
turueee <turushkine@inbox.ru>
2024-12-19 10:38:31 +0300

    Update task3.c

--- 3824Б1ФИ1/27 Турушкин Егор Антонович/task3.c
+++ 3824Б1ФИ1/27 Турушкин Егор Антонович/task3.c
@@ -1,7 +1,22 @@
 // Задача 3. Дана строка s. Сформировать новую строку, состоящую только из 
 // заглавных букв английского алфавита, содержащихся в исходной строке s. 
 // Пример: s = "aBc1D2e34F56g", ответ: "BDF".
-char* task3(char *s)
+#include <stdlib.h>
+
+char* task3(char* s)
 {
-  return "task3";
+  int i = 0, counter = 0;
+  char* res;
+  (res) = (char*)malloc(sizeof(char)*256);
+  while ((s[i]) != 0)
+  {
+    if ((s[i] >= 65) && (s[i] <= 90))
+    {
+      res[counter] = s[i];
+      counter++;
+    }
+    i++;
+  }
+  res[counter] = 0;
+  return res;
 }
