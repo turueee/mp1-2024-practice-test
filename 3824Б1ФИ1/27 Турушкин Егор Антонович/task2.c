ed779954a404c4db4a59ebe06f94eb4785b2aea0
turueee <turushkine@inbox.ru>
2024-12-19 10:33:26 +0300

    Update task2.c

--- 3824Б1ФИ1/27 Турушкин Егор Антонович/task2.c
+++ 3824Б1ФИ1/27 Турушкин Егор Антонович/task2.c
@@ -2,7 +2,30 @@
 // Найти значение минимального из его локальных минимумов.
 // Локальный минимум – элемент, который <= левого и правого соседей.
 // При отсутствии локальных минимумов вернуть -1.
+#include <limits.h>
+
 long long task2(unsigned A[], size_t n)
 {
-  return -2;
+  int k = 0,i = 0;
+  long long min = LLONG_MIN;
+  for (i = 1; i < n - 1; i++)
+  {
+    if (A[i - 1] >= A[i] && A[i] <= A[i + 1])
+    {
+      if (k == 0)
+      {
+        min = A[i];
+        k++;
+      }
+      else
+      {
+        if (A[i] < min)
+          min = A[i];
+      }
+    }
+  }
+  if (k == 0)
+    return -1;
+  else
+    return min;
 }
